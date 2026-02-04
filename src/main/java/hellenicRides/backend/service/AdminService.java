d3e2a017a5f9e9a554d4707b7a9bc39834e43a20
elias <elias.eloudghiri@gmail.com>
2026-02-04 11:25:31 +0100

    Ajout envoie de mail

--- src/main/java/hellenicRides/backend/service/AdminService.java
+++ src/main/java/hellenicRides/backend/service/AdminService.java
@@ -3,7 +3,9 @@ package hellenicRides.backend.service;
 import hellenicRides.backend.dto.AuthResponse;
 import hellenicRides.backend.entity.AdminPost;
 import hellenicRides.backend.util.JwtUtil;
+import java.io.File;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,11 +13,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AdminService {
 
   private final AuthenticationManager authenticationManager;
   private final CustomUserDetailsService userDetailsService;
   private final JwtUtil jwtUtil;
+  private final EmailService emailService;
 
   public AuthResponse login(AdminPost adminPost) throws Exception {
     try {
@@ -28,6 +32,19 @@ public class AdminService {
     final UserDetails userDetails = userDetailsService.loadUserByUsername(adminPost.getEmail());
 
     final String jwt = jwtUtil.generateToken(userDetails);
+
+    try {
+      File f = new File("C:\\Users\\elias\\Downloads\\Cahier des charges - Gemini.pdf");
+      emailService.sendEmailWithAttachment(
+          "elias.eloudghiri@gmail.com",
+          "Admin Login Alert",
+          "Admin with email " + adminPost.getEmail() + " has logged in.",
+          f);
+
+    } catch (Exception e) {
+      log.error("Failed to send admin login alert email.");
+      log.error("Error: {}", e.getMessage());
+    }
 
     return new AuthResponse(jwt);
   }
